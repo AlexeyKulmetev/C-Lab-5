3f85b11e44c4d8b282ce24e88386cd122608a29b
AlexeyKulmetev <44574285+AlexeyKulmetev@users.noreply.github.com>
2022-07-16 19:06:27 +0300

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -8,13 +8,15 @@ int main() {
 
 	char str_in[N];
 	char str_out[N];
-
 	
 	printf("Enter text: ");
 
 	fgets(str_in, N, stdin);
 
 	randomWords(str_in, str_out);
+
+	printf("\n");
+	puts(str_out);
 
 	return 0;
 }
