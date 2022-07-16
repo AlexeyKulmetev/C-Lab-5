d86dfda0278334d065b9061d4f75a97d827ceb2d
AlexeyKulmetev <44574285+AlexeyKulmetev@users.noreply.github.com>
2022-07-16 19:06:32 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -13,6 +13,7 @@ char* randomWords(char* in, char* out) {
 	int randNum = 0;
 	int pIndex = 0;
 	int i = 0;
+	int newStrInd = 0;
 
 	while (in[i] != '\0' && in[i] != '\n') {
 		if (in[i] >= 'A' && in[i] <= 'Z' || in[i] >= 'a' && in[i] <= 'z') {
@@ -31,11 +32,11 @@ char* randomWords(char* in, char* out) {
 		printf(" %c ", *pArray[i]);
 		i++;
 	}
-
 	printf("\n");
 	i = 0;
 	// FISHER-YATES MIXING
 	srand(time(NULL));
+
 	for (int i = 0; i < pIndex; i++) {
 		randNum = rand() % pIndex;
 		tmpPointer = pArray[i];
@@ -52,6 +53,17 @@ char* randomWords(char* in, char* out) {
 	}
 
 	// FORM A NEW STRING USING JUMBLED POINTERS
+	for (int i = 0; i < pIndex; i++) {
+		tmpPointer = pArray[i];
+		for (int j = 0; tmpPointer[j] != ' ' && tmpPointer[j] != '\0' && tmpPointer[j] != '\n'; j++) {
+			out[newStrInd++] = tmpPointer[j];
+		}
+		if (i < pIndex - 1) {
+			out[newStrInd++] = ' ';
+		}
+
+	}
+	out[newStrInd] = '\0';
 
 	return out;	
-}
+} 
