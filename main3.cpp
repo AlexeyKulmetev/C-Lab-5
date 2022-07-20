f760d4ca6a56aabcf3b93a6d3017b41951252e51
AlexeyKulmetev <44574285+AlexeyKulmetev@users.noreply.github.com>
2022-07-20 19:20:54 +0300

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include "task3.h"
 
+#define N 1000
 int main() {
 
 	FILE* filePointer;
-
+	char in_str[N];
+	char out_str[N];
+	
 	filePointer = fopen("text.txt", "r");
-
+	while (fgets(in_str, N, filePointer) != NULL) {
+		mixLine(in_str, out_str);
+		printf("%s", out_str);
+	}
 
 
 }
