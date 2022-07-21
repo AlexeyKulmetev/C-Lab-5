ffcd2d8e91f5b5b79b5ffce7346878966352fdce
AlexeyKulmetev <44574285+AlexeyKulmetev@users.noreply.github.com>
2022-07-21 16:58:05 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -7,14 +7,14 @@
 #define M 50
 char* mixChars(char* in, char* out) {
 
-	char in[M];
 	char tmp;
 	int i = 0;
 	int randInd = 0;
 	int length = 0;
 
 	// COUNTING THE LENGTH OF THE WORD
-	while (in[i] != '\0') {
+	for (int i = 0; in[i] != '\0' && in[i] != '\n' && in[i] != ' '; i++) {
+		out[i] = in[i];
 		length++;
 		i++;
 	}
@@ -22,32 +22,39 @@ char* mixChars(char* in, char* out) {
 	srand(time(NULL));
 	for (int i = 1; i < length - 1; i++) {
 		randInd = rand() % (length - 2) + 1;
-		tmp = in[i];
-		in[i] = in[randInd];
-		in[randInd] = tmp;
-		printf("\n%s", in);
-	}	
-	printf("\n%s", in);
+		tmp = out[i];
+		out[i] = out[randInd];
+		out[randInd] = tmp;
+		printf("\n%s", out);
+	}
+	printf("\n%s", out);
+	return out;
 }
 
 char* mixLine(char* instr, char* outstr) {
 
 	char* word;
-	char out_word[70];
+	char out_word[100];
 	int i = 0;
-	int out_word_ind = 0;
+	int out_str_ind = 0;
 
-	// SPLITTING A STRING INTO WORDS
+	// SPLITTING THE STRING INTO WORDS
 	while (instr[i] != '\0' && instr[i] != '\n') {
-		if (instr[i] >= 'A' && instr[i] <= 'Z' || instr[i] >= 'a' && instr[i] <= 'z') {
+		if (instr[i] >= 'A' && instr[i] <= 'Z' || instr[i] >= 'a' && instr[i] <= 'z') {	
 			word = &instr[i];
-			while (instr[i] >= 'A' && instr[i] <= 'Z' || instr[i] >= 'a' && instr[i] <= 'z')
-				i++;
 			mixChars(word, out_word);
-			outstr
+			while (instr[i] >= 'A' && instr[i] <= 'Z' || instr[i] >= 'a' && instr[i] <= 'z') {				
+				i++;
+			}			
+			// FORM THE OUTPUT STRING
+			for (int i = 0; out_word[i] != '\0'; i++) {
+				outstr[out_str_ind++] = out_word[i];
+			}
+			if (instr[i + 1] != '\0' || instr[i + 1] != '\n') {
+				outstr[out_str_ind++] = ' ';
+			}
 		}
 		else i++;
-
 	}
-
+	return outstr;
 }
