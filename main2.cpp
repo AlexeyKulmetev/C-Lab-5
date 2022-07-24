6469cbad5dfcb966fe65bee1be65db1d0c045cf5
AlexeyKulmetev <44574285+AlexeyKulmetev@users.noreply.github.com>
2022-07-24 18:17:45 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,10 +1,21 @@
-#include <stdio.h>
-
+#include "task2.h"
+#include <Windows.h>
 
 #define M 20
 
 int main() {
 
-	char arr[M][M];
+	char matrix[M][M];
+	char pArray[M];
 
+	while (1) {
+		clearMatrix(matrix);
+		fillMatrix(matrix);
+		settMatrix(matrix);
+		system("cls");
+		printMatrix(matrix);
+		Sleep(2000);
+	}
+
+	return 0;
 }
